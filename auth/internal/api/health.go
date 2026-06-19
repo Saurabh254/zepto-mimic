9fff224e4dcc2714a31d8bee81cab021ced8869a
saurabh254 <sauravvishwakarma030@gmail.com>
2026-06-20 01:30:24 +0530

    chore(cleanup): removed unused files

--- auth/internal/api/health.go
+++ auth/internal/api/health.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func RegisterHealthRoutes(rg *gin.RouterGroup) {
 
 	rg.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+  		c.JSON(200, gin.H{
 			"status": "ok",
 		})
 	})
